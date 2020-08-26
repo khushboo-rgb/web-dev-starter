c79cefc42c33a53be3a1fe73ac9145df52deabc7
khushboo shrivastava <69577250+khushboo-rgb@users.noreply.github.com>
2020-08-26 15:41:26 +0530

    Issue Tracker App Demo is Commited

---
+++ Issue-Tracker-App/main.js
@@ -0,0 +1,90 @@
+
+document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
+
+function saveIssue(e){
+  var issueDesc = document.getElementById('issueDescInput').value;
+  var issueSeverity = document.getElementById('issueSeverityInput').value;
+  var issueAssignedTo = document.getElementById('issueAssignedToInput').value;
+  var issueId = chance.guid();//this function returns global unique identifier and we get this feature from chance.js library
+   var issueStatus = "Open";
+
+   //creating issue object
+   var issue = {
+       id: issueId,
+       description: issueDesc,
+       severity: issueSeverity,
+       assignedTo: issueAssignedTo,
+       status: issueStatus
+   }
+
+   if(localStorage.getItem('issues') == null)
+   {
+       var issues = [];
+       issues.push(issue);
+       localStorage.setItem('issues', JSON.stringify(issues));
+   }
+   else{
+       var issues = JSON.parse(localStorage.getItem('issues'));
+       issues.push(issue);
+       localStorage.setItem('issues', JSON.stringify(issues));
+   }
+
+   document.getElementById('issueInputForm').reset();
+   fetchIssues();
+
+   e.preventDefault();
+   
+}
+function setStatusClosed(id){
+  var issues = JSON.parse(localStorage.getItem('issues'));
+  for ( var i = 0; i < issues.length; i++)
+  {
+      if(issues[i].id == id)
+      {
+          issues[i].status = 'Closed';
+      }
+  }
+  localStorage.setItem('issues', JSON.stringify(issues));
+  fetchIssues();
+
+}
+
+function deleteIssue(id)
+{
+    var issues = JSON.parse(localStorage.getItem('issues'));
+  for ( var i = 0; i < issues.length; i++)
+  {
+      if(issues[i].id == id)
+      {
+         issues.splice(i, 1);
+      }
+  }
+  localStorage.setItem('issues', JSON.stringify(issues));
+  fetchIssues();
+}
+
+function fetchIssues(){
+    var issues = JSON.parse(localStorage.getItem('issues'));
+    var issuesList = document.getElementById('issuesList');
+
+    issuesList.innerHTML = '';
+
+    for ( var i = 0; i < issues.length; i++){
+        var id = issues[i].id;
+        var desc = issues[i].description;
+        var severity = issues[i].severity;
+        var assignedTo = issues[i].assignedTo;
+        var status = issues[i].status;
+
+        issuesList.innerHTML += '<div class="well">' +
+                                 '<h6>Issue Id:' + id + '</h6>' +
+                                 '<p><span class="label label-info">' + status + '</span></p>' +
+                                 '<h3>' + desc + '</h3>' +
+                                 '<p><span class="glyphicon glyphicon-time"></span>' + severity + '</p>' +
+                                 '<p><span class="glyphicon glyphicon-user"></span>' + assignedTo + '</p>' +
+                                 '<a href="#" onClick = "setStatusClosed(\''+id+'\');" class="btn btn-warning">Close</a>' + "  "+ 
+                                 '<a href="#" onClick = "deleteIssue(\''+id+'\'); "  class="btn btn-danger">Delete</a>' + 
+                                 '</div>';
+    }
+
+}
